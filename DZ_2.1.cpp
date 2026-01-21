d493a5870af2c03c434aeef13f4deff74384a741
avtodiagnostikakzn-dot <avtodiagnostika.kzn@gmail.com>
2026-01-21 22:28:11 +0300

    Add files via upload

---
+++ DZ_2.1.cpp
@@ -0,0 +1,13 @@
+﻿
+#include <iostream>
+#include <string>  
+
+int main()
+{
+    std::string word;
+    std::cout << "Введите слово: " << "\n";
+    std::cin >> word;
+    std::cout << "Вы ввели: " << word << std::endl << "\n";
+
+    return EXIT_SUCCESS;
+}
---
+++ DZ_2.2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <string>  
+
+int main()
+{
+    std::string word;
+    std::cout << "Введите слово: " << "\n";
+    std::cin >> word;
+    std::cout << "Вы ввели: " << "\n" << word << std::endl;
+
+    return EXIT_SUCCESS;
+}
