0c842bdf06a0b73a9db17a5bc6da94084dbd057c
avtodiagnostikakzn-dot <avtodiagnostika.kzn@gmail.com>
2026-01-22 16:08:00 +0300

    Update DZ_2.2.cpp
    Изменено

--- DZ_2.2.cpp
+++ DZ_2.2.cpp
@@ -6,8 +6,7 @@ int main()
     std::string line;
     std::cout << "Введите строку: " << std::endl;
     std::getline(std::cin, line);
-    std::cout << "Вы ввели: " << std::endl << line;
+    std::cout << "Вы ввели: " << line << std::endl;
     
     return EXIT_SUCCESS;
 }
-
